935b41906f1cd502053afafd7f85966ccf674517
dungthai85 <Dungthai85@gmail.com>
2020-02-12 18:39:52 -0800

    merge

--- js/unitscontrol.js
+++ js/unitscontrol.js
@@ -98,20 +98,6 @@ UnitsControl.prototype.update = function () {
             }
         }
     }
-<<<<<<< HEAD
-    if (is_enemy_spawn_1) {
-       // this.game.addEntity(new Orc(this.game,AM.getAsset("./img/enemy_team/orc/orc.png"), 1000, 370));
-       // this.game.addEntity(new ReaperMan(this.game,AM.getAsset("./img/enemy_team/reaper_chibbi/reaper.png"), 1000, 455));
-        this.game.addEntity(new FallenAngel(this.game,AM.getAsset("./img/enemy_team/fallen_angel/fallen_angel.png"), 1000, 535));
-        is_enemy_spawn_1 = false;
-    }
-    else if (is_enemy_spawn_2) {
-        this.game.addEntity(new Orc(this.game,AM.getAsset("./img/enemy_team/orc/orc.png"), 1000, 370));
-       // this.game.addEntity(new ReaperMan(this.game,AM.getAsset("./img/enemy_team/reaper_chibbi/reaper.png"), 1000, 455));
-       // this.game.addEntity(new FallenAngel(this.game,AM.getAsset("./img/enemy_team/fallen_angel/fallen_angel.png"), 1000, 535));
-        is_enemy_spawn_2 = false;
-    }
-=======
     
     // if (is_enemy_spawn_1) {
     //     this.game.addEntity(new Orc(this.game,AM.getAsset("./img/enemy_team/orc/orc.png"), 1000, 370));
@@ -125,7 +111,6 @@ UnitsControl.prototype.update = function () {
     //     this.game.addEntity(new FallenAngel(this.game,AM.getAsset("./img/enemy_team/fallen_angel/fallen_angel.png"), 1000, 535));
     //     is_enemy_spawn_2 = false;
     // }
->>>>>>> master
     Entity.prototype.update.call(this);
 }
 
